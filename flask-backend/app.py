6779254b31f110520b66d32dd97714f0e9d331f6
VikiMaiselman <maiselmanwikki@gmail.com>
2024-01-09 13:11:16 +0200

    flask backend update appropriately to support CRUD

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,16 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins=["*"], supports_credentials=True)
 
-@app.route("/")
-def home():
-    return "<h1>Hello, Flask!</h1>"
-
-@app.route("/topics")
-def topics():
-    rows = [
+rows_topics = [
     {
       "id": 1,
       "topicName": "A",
@@ -25,11 +19,8 @@ def topics():
     { "id": 6, "topicName": "F", "batchingTime": None, "batchingNumber": 150 },
     { "id": 7, "topicName": "G", "batchingTime": 5, "batchingNumber": 44 },
   ]
-    return rows
 
-@app.route("/devices")
-def devices():
-    rows = [
+rows_devices = [
     {
       "id": 1,
       "deviceName": "A",
@@ -44,4 +35,68 @@ def devices():
     { "id": 6, "deviceName": "F", "batchingTime": None, "batchingNumber": 50 },
     { "id": 7, "deviceName": "G", "batchingTime": 55, "batchingNumber": 44 },
   ]
-    return rows
+
+@app.route("/")
+def home():
+    return "<h1>Hello, Flask!</h1>"
+
+@app.route("/topics", methods=['GET', 'POST', 'PATCH'])
+def topics():
+    if request.method == 'GET':
+        return rows_topics
+    
+    if request.method == 'POST':
+        new_item = request.json["new_item"]
+
+        if not new_item or not new_item["topicName"] \
+            or not new_item["batchingTime"] or not new_item["batchingNumber"]:
+            return "Failure"
+        
+        item_to_add = {
+            "id": rows_topics.__len__() + 1,
+            "topicName": new_item["topicName"],
+            "batchingTime": new_item["batchingTime"], 
+            "batchingNumber": new_item["batchingNumber"]
+        }
+        rows_topics.append(item_to_add)
+        return "Success"
+    
+    if request.method == "PATCH":
+        updated_item = request.json["updated_item"]
+        item_in_list = find_dict_by_id(rows_topics, updated_item["id"])
+
+        item_in_list.update(updated_item)
+        return "Success"
+    
+    # if request.method == "DELETE": 
+    #     print(request.json)
+    #     item_to_delete_id = request.json["item_to_delete_id"]
+    #     print(request.json)
+    #     for i in range(len(rows_topics)):
+    #         if rows_topics[i]['id'] == item_to_delete_id:
+    #             del rows_topics[i]
+    #             return "Success"
+
+    #     return "Failure"
+
+@app.route("/topics/<int:item_to_delete_id>", methods=['DELETE'])
+def topics_del(item_to_delete_id):
+    print(item_to_delete_id)
+    for i in range(len(rows_topics)):
+        if rows_topics[i]['id'] == item_to_delete_id:
+            del rows_topics[i]
+            return "Success"
+
+    return "Failure"
+
+
+@app.route("/devices")
+def devices():
+    return rows_devices
+
+
+def find_dict_by_id(lst, target_id):
+    for dic in lst:
+        if dic['id'] == target_id:
+            return dic
+    return None  # Return None if the id is not found
